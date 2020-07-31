4dc0cfb6323f5435a80ed1bbafd95ba96645fc72
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-07-31 16:59:01 -0700

    Finish artists route and artists search route

--- app.py
+++ app.py
@@ -141,9 +141,7 @@ def venues():
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
     search = request.form.get('search_term', '')
-
     venues = Venue.query.filter(Venue.name.ilike(f'%{search}%')).all()
-
     data = []
 
     for venue in venues:
@@ -244,34 +242,44 @@ def delete_venue(venue_id):
 
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    artists = Artist.query.all()
+    data = []
+
+    for artist in artists:
+        data.append({
+            'id': artist.id,
+            'name': artist.name
+        })
+
     return render_template('pages/artists.html', artists=data)
 
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    search = request.form.get('search_term', '')
+    artists = Artist.query.filter(Artist.name.ilike(f'%{search}%')).all()
+    data = []
+
+    for artist in artists:
+        shows = artist.shows
+        upcoming_shows = []
+
+        for show in shows:
+            if show.start_time > current_time:
+                upcoming_shows.append(show)
+
+        data.append({
+            'id': artist.id,
+            'name': artist.name,
+            'num_upcoming_shows': len(upcoming_shows)
+        })
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        'count': len(artists),
+        'data': data
     }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+
+    return render_template('pages/search_artists.html', results=response, search_term=search)
 
 
 @app.route('/artists/<int:artist_id>')
