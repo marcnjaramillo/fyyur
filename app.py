3358c3a46f725660e356940f5b20229f2f7bb847
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-08-12 22:28:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ class Show(db.Model):
 def format_datetime(value, format="medium"):
     date = dateutil.parser.parse(value)
     if format == "full":
-        format = "EEEE, MMMM d, y "at" h:mma"
+        format = "EEEE, MMMM d, y 'at' h:mma"
     elif format == "medium":
         format = "EE, MM dd, y h:mma"
     return babel.dates.format_datetime(date, format)
